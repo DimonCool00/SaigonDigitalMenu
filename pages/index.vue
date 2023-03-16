98774db70f1ebce023445ac02eea2088b3429444
DimonCool00 <smirdimon2000@mail.ru>
2023-03-16 10:56:46 +0600

    banner and scroll top

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <mobile-app-banner></mobile-app-banner>
     <VueHeader />
     <Products />
   </main>
@@ -9,10 +10,12 @@
 <script>
 import Products from "~/components/Products.vue";
 import VueHeader from '~/components/ui/vueHeader.vue';
+import MobileAppBanner from "../components/MobileAppBanner.vue";
 export default {
   components: {
     Products,
-    VueHeader
+    VueHeader,
+    MobileAppBanner
   }
 }
 </script>
