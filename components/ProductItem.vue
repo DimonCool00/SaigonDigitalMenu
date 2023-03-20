0ff50d40cb032b4e7cb7565c0770217821b7fce7
DimonCool00 <smirdimon2000@mail.ru>
2023-03-20 10:10:54 +0600

    nav menu and refractoring code

--- components/ProductItem.vue
+++ components/ProductItem.vue
@@ -1,69 +1,99 @@
 <template>
-  <div class="d-flex flex-column w390" :id="item.scrollId">
-    <h3 class="category-name">{{item.name}}</h3>
+  <div class="d-flex flex-column w390" v-if="products.length">
+    <h3 class="category-name">{{ categoryName }}</h3>
     <div class="mt-4 d-flex flex-column">
-      <div class="product-item d-flex" v-for="product in item.products" :key="product.name" @click="openModal(product)">
-        <img v-if="product.images.length && product.images[0]" :src="`https://testapi.rst.ozo.direct${product.images[0].imagePath}`" class="product-item__image"/>
-        <img v-else src="@/assets/images/notload.png" class="product-item__image"/>
+      <div
+        class="product-item d-flex"
+        v-for="product in products"
+        :key="product.name"
+        @click="openModal(product)"
+      >
+        <img
+          v-if="product.images.length && product.images[0]"
+          :src="`https://testapi.rst.ozo.direct${product.images[0].imagePath}`"
+          class="product-item__image"
+        />
+        <img v-else src="@/assets/images/notload.png" class="product-item__image" />
         <div class="product-item__details d-flex flex-column justify-space-between">
-          <h3>{{product.name}}</h3>
-                <span>{{product.description}}</span>
-                <button @click="price(product)">
-                  {{product.price}} ₸
-                </button>
+          <h3>{{ product.name }}</h3>
+          <span>{{ product.description }}</span>
+          <button @click="price(product)">{{ product.price }} ₸</button>
         </div>
       </div>
     </div>
     <v-dialog v-if="selectedProduct.images" content-class="product-popup" v-model="showPopup">
-      <div class="modal-line d-flex align-center"  @click="closeModal"></div>
+      <div class="modal-line d-flex align-center" @click="closeModal"></div>
       <div class="product-popup__header d-flex justify-space-between align-center">
-        <h2 class="product-popup__category">{{item.name}}</h2>
-        <svg @click="closeModal" class="close" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M15 30C23.2843 30 30 23.2843 30 15C30 6.71573 23.2843 0 15 0C6.71573 0 0 6.71573 0 15C0 23.2843 6.71573 30 15 30Z" fill="#EFEFEF"/>
-          <path d="M9.90625 19C9.52344 19.375 9.51562 20.0625 9.91406 20.4609C10.3125 20.8594 11 20.8438 11.375 20.4688L15 16.8438L18.625 20.4609C19.0156 20.8516 19.6875 20.8594 20.0859 20.4531C20.4844 20.0547 20.4844 19.3828 20.0938 18.9922L16.4766 15.375L20.0938 11.75C20.4844 11.3594 20.4844 10.6875 20.0859 10.2891C19.6875 9.88281 19.0156 9.89062 18.625 10.2812L15 13.8984L11.375 10.2734C11 9.89844 10.3125 9.88281 9.91406 10.2812C9.51562 10.6797 9.52344 11.3672 9.90625 11.75L13.5312 15.375L9.90625 19Z" fill="#9F9F9F"/>
+        <h2 class="product-popup__category">{{ categoryName }}</h2>
+        <svg
+          @click="closeModal"
+          class="close"
+          width="30"
+          height="30"
+          viewBox="0 0 30 30"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M15 30C23.2843 30 30 23.2843 30 15C30 6.71573 23.2843 0 15 0C6.71573 0 0 6.71573 0 15C0 23.2843 6.71573 30 15 30Z"
+            fill="#EFEFEF"
+          />
+          <path
+            d="M9.90625 19C9.52344 19.375 9.51562 20.0625 9.91406 20.4609C10.3125 20.8594 11 20.8438 11.375 20.4688L15 16.8438L18.625 20.4609C19.0156 20.8516 19.6875 20.8594 20.0859 20.4531C20.4844 20.0547 20.4844 19.3828 20.0938 18.9922L16.4766 15.375L20.0938 11.75C20.4844 11.3594 20.4844 10.6875 20.0859 10.2891C19.6875 9.88281 19.0156 9.89062 18.625 10.2812L15 13.8984L11.375 10.2734C11 9.89844 10.3125 9.88281 9.91406 10.2812C9.51562 10.6797 9.52344 11.3672 9.90625 11.75L13.5312 15.375L9.90625 19Z"
+            fill="#9F9F9F"
+          />
         </svg>
       </div>
       <div class="product-popup__content">
-        <img v-if="selectedProduct.images.length && selectedProduct.images[0]" :src="`https://testapi.rst.ozo.direct${selectedProduct.images[0].imagePath}`"/>
+        <img
+          v-if="selectedProduct.images.length && selectedProduct.images[0]"
+          :src="`https://testapi.rst.ozo.direct${selectedProduct.images[0].imagePath}`"
+        />
         <img v-else src="@/assets/images/notload.png" />
         <h3 class="product-popup__title mb-3 mt-6">
-          {{selectedProduct.name}}
+          {{ selectedProduct.name }}
         </h3>
         <span class="product-popup__description">
-          {{selectedProduct.description}}
+          {{ selectedProduct.description }}
         </span>
       </div>
     </v-dialog>
-   </div>
+  </div>
 </template>
 
 <script>
 export default {
   props: {
-    item: {
-      type: Object,
-      default: {}
+    products: {
+      type: Array,
+      default: [],
+    },
+    categoryName: {
+      type: String,
+      default: '',
     },
   },
   data() {
-      return {
-        showPopup: false,
-        selectedProduct: {}
-      }
+    return {
+      showPopup: false,
+      selectedProduct: {},
+    };
   },
   methods: {
     price(product) {
-      console.log(product)
+      console.log(product);
     },
     openModal(item) {
       this.showPopup = true;
-      this.selectedProduct = item
+      this.selectedProduct = item;
     },
     closeModal() {
-      this.showPopup = false
-    }
-  }
-}
+      this.showPopup = false;
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -73,16 +103,16 @@ export default {
   margin-bottom: 35px;
 }
 .modal-line {
-    background: #CDCDCD;
-    border-radius: 10px;
-    border: 1px ;
-    height: 5px;
-    width: 39px;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    margin: auto;
-    margin-bottom: 5px;
+  background: #cdcdcd;
+  border-radius: 10px;
+  border: 1px;
+  height: 5px;
+  width: 39px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin: auto;
+  margin-bottom: 5px;
 }
 .category-name {
   font-family: 'Inter';
@@ -109,7 +139,7 @@ export default {
   &__details {
     h3 {
       display: -webkit-box;
-      -webkit-line-clamp: 1;
+      -webkit-line-clamp: 2;
       -webkit-box-orient: vertical;
       overflow: hidden;
     }
@@ -124,10 +154,10 @@ export default {
       font-weight: 400;
       font-size: 14px;
       line-height: 17px;
-      color: #9F9F9F;
+      color: #9f9f9f;
     }
     button {
-      background: #F0F1F3;
+      background: #f0f1f3;
       border-radius: 5px;
       max-width: 107px;
       width: 100%;
@@ -174,8 +204,7 @@ export default {
     line-height: 19px;
     display: flex;
     align-items: center;
-    color: #5E5E5E;
-
+    color: #5e5e5e;
   }
   &__content {
     margin-top: 15px;
--- components/Products.vue
+++ components/Products.vue
@@ -1,222 +1,201 @@
 <template>
   <div class="products">
     <div class="search-component">
-      <input type="search" class="search-component__input" v-model="search" placeholder="Поиск..." @keyup="filterProducts"/>
+      <input
+        type="search"
+        class="search-component__input"
+        v-model="search"
+        placeholder="Поиск..."
+      />
       <svg width="17" height="17" class="search-component__icon">
         <use href="@/assets/icons/icons.svg#search-icon"></use>
       </svg>
     </div>
-    <div class="navigation d-flex align-center">
-      <div v-for="item in result" :key="item.id" @click="selectCategory(item)" :class="{active: selectedCategory === item.name}">
-        <h3>{{item.name}}</h3>
-      </div>
-    </div>
-    <ProductItem v-for="(item) in currentItems" :key="item.id" :item="item" :showPopup="showPopup"/>
+
+    <scrollactive class="navigation d-flex align-center"
+      v-on:itemchanged="onItemChanged"
+      active-class="active"
+      :modifyUrl="false"
+      :offset="80"
+      :duration="800"
+      bezier-easing-value=".5,0,.35,1"
+    >
+      <a
+        class="scrollactive-item"
+        v-for="category in filteredCategories"
+        :key="category.id"
+        :href="'#' + category.id"
+      >
+        <h3 v-text="category.name"></h3>
+      </a>
+    </scrollactive>
+
+    <ProductItem
+      v-for="[categoryId, products], idx in Object.entries(filteredItems)"
+      :key="idx"
+      :id="categoryId"
+      :products="products"
+      :categoryName="getCategoryMeta(categoryId).name"
+      :showPopup="showPopup"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        products: [],
-        showModal: false,
-        accessToken: '',
-        showPopup: false,
-        search: '',
-        categories: [],
-        selectedCategory: null,
-        timer: null,
-        result: [],
-        filteredItems: []
-      }
+export default {
+
+  data() {
+    return {
+      products: [],
+      showModal: false,
+      accessToken: '',
+      showPopup: false,
+      search: '',
+      categories: [],
+    };
+  },
+  async created() {
+    await this.init();
+  },
+  computed: {
+    filteredCategories() {
+      return this.categories.filter(({ id }) => this.items[id].length);
     },
-   async created() {
-    await this.getAuthToken()
+
+    items() {
+      const products = this.products.reduce((acc, product) => ({
+        ...acc,
+        [product.categoryId]: [
+          ...(acc[product.categoryId] ?
+          [...acc[product.categoryId], product] :
+          [product])
+        ],
+      }), {});
+      return this.categories.reduce((acc, { id }) => ({
+        ...acc,
+        [id]: products[id] || []
+      }), {})
     },
-    computed: {
-      currentItems() {
-        return this.filteredItems.some(item => item.products.length) ? this.filteredItems : this.result
-      }
-    },
-    methods: {
-      filterProducts(event) {
-        this.filteredItems = this.result.map(res => {
-          return ({
-            ...res,
-            products: res.products.filter(item => item.name.toLowerCase().includes(event.target.value.toLowerCase()))
-          })
-        })
-      },
-      setProductsList() {
-        console.log(this.products, this.categories)
-          for(let j = 0; j < this.products.length; j++) {
-            for(let i = 0; i < this.categories.length; i++) {
-            if(this.categories[i].id === this.products[j].categoryId) {
-              this.result.push({
-                name: this.categories[i].name,
-                id: this.categories[i].id,
-                scrollId: `Category${i}`,
-                products: []
-              })
-            }
+
+    filteredItems() {
+      return Object.fromEntries(
+        Object.entries(this.items).map(([key, value]) => {
+          if (value) {
+            return [
+              key,
+              value.filter(({ name }) => name.toLowerCase().includes(this.search.toLowerCase()))
+            ];
           }
-            this.result.forEach(item => {
-              if(item.id === this.products[j].categoryId) {
-                item.products.push({
-                  name: this.products[j].name,
-                  description: this.products[j].description,
-                  images: this.products[j].images,
-                  price: this.products[j].price,
-                  id: this.products[j].id
-                })
-              }
-              switch (item.name) {
-                case "Завтраки" : {
-                  item.position = 1;
-                  break;
-                }
-                case 'Салаты и закуски': {
-                  item.position = 2;
-                  break;
-                }
-                case 'Супы': {
-                  item.position = 3;
-                  break;
-                }
-                case 'Мясо и тофу' : {
-                  item.position = 4
-                  break;
-                }
-                case 'Морепродукты и рыба': {
-                  item.position = 5
-                  break;
-                }
-                case 'Лапша и рис': {
-                  item.position = 6;
-                  break;
-                }
-                case 'Суши' : {
-                  item.position = 7
-                  break;
-                }
-                case 'Гарниры': {
-                  item.position = 8;
-                  break;
-                }
-                case 'Десерты': {
-                  item.position = 9;
-                  break
-                }
-                case 'Хлеб': {
-                  item.position = 10;
-                  break;
-                }
-                case 'Безалкогольные напитки': {
-                  item.position = 11;
-                  break;
-                }
-                case 'Бутылочное пиво' : {
-                  item.position = 12;
-                  break;
-                }
-                case 'Закуски к пиву': {
-                  item.position = 13;
-                  break;
-                }
-              }
-            })
-        }
-        this.result = this.result.filter((v, i, a) => a.findIndex(v2 => (v2.id === v.id)) === i).sort((a,b) => {
-          return a.position - b.position
         })
-        console.log(this.result)
-      },
-     async getAuthToken() {
-        const body = {
-          appName: "menu.rst.ozo.direct",
-        };
-        const options = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
-        };
-        fetch("/api/Auth/GetApplicationToken", options)
-          .then((response) => {
-            if (!response.ok) {
-              throw new Error("Failed to get auth token");
-            }
-            return response.json();
-          })
-          .then((data) => {
-            this.accessToken = data.accessToken
-            this.getRestaurantProducts()
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-      selectCategory(item) {
-        this.selectedCategory = item.name
-        if(process.client) {document.getElementById(item.scrollId).scrollIntoView();
-        }
-      },
-     async getRestaurantProducts(event, bool) {
-          const url = `/api/product/GetRestaurantProducts?restaurantId=55DE9A40-561E-4F44-9AFF-9A8D048165FA`;
-          const options = {
-            headers: {
-              Authorization: `Bearer ${this.accessToken}`,
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-              "Access-Control-Allow-Headers":
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-            },
-          };
-          fetch(url, options)
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Failed to get restaurant products");
-              }
-              return response.json();
-            })
-            .then((data) => {
-            this.products = data;
-            this.getCategories()
-            })
-            .catch((error) => {
-              console.error(error);
-            });
-      },
-    async getCategories() {
-          const url = `/api/Category/GetRestaurantCategories?restaurantId=55DE9A40-561E-4F44-9AFF-9A8D048165FA`;
-          const options = {
-            headers: {
-              Authorization: `Bearer ${this.accessToken}`,
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-              "Access-Control-Allow-Headers":
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-            },
-          };
-          fetch(url, options)
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Failed to get restaurant products");
-              }
-              return response.json();
-            }).then(res => {
-                this.categories = res
-            this.setProductsList()
-            }
-          )
-      }
+      )
+    },
+  },
+
+  watch: {
+    currentCategory() {
+      // this.currentCategory.scrollTo({ behavior: 'smooth', inline: 'center' });
     }
-  }
+  },
+
+  methods: {
+    onItemChanged(event, currentItem, lastActiveItem) {
+    },
+    async init() {
+      await this.getAuthToken();
+      await this.getRestaurantProducts();
+      await this.getCategories();
+    },
+
+    getCategoryMeta(categoryId) {
+      return this.categories.find(({ id }) => id == categoryId);
+    },
+
+    async getAuthToken() {
+      const body = {
+        appName: 'menu.rst.ozo.direct',
+      };
+      const options = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      };
+      try {
+        const response = await fetch('/api/Auth/GetApplicationToken', options);
+        const { accessToken } = await response.json();
+        this.accessToken = accessToken;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getRestaurantProducts() {
+      const url = `/api/product/GetRestaurantProducts?restaurantId=55DE9A40-561E-4F44-9AFF-9A8D048165FA`;
+      const options = {
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers':
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        },
+      };
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error('Failed to get restaurant products');
+        }
+        const data = await response.json();
+        this.products = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getCategories() {
+      const url = `/api/Category/GetRestaurantCategories?restaurantId=55DE9A40-561E-4F44-9AFF-9A8D048165FA`;
+      const options = {
+        headers: {
+          Authorization: `Bearer ${this.accessToken}`,
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers':
+            'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+        },
+      };
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error('Failed to get restaurant products');
+        }
+        const data = await response.json();
+        this.categories = data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+
+  mounted() {
+    window.addEventListener('scroll', () => {
+      const el = document.querySelector('.scrollactive-item.active');
+      if (el) {
+        this.$nextTick(() => {
+          el.scrollIntoViewIfNeeded({ behavior: 'smooth', inline: 'center' });
+        })
+      }
+    });
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.navigation::-webkit-scrollbar {
+  height: 0px;
+  background-color: #ffffff;
+}
+.navigation::-webkit-scrollbar-thumb {
+  background-color: #ffffff;
+}
 .search-component {
   max-width: 390px;
   width: 100%;
@@ -227,12 +206,12 @@
   position: relative;
 
   &__input {
-    background: #F0F1F3;
-    border-radius: 8px;
+    background: #f0f1f3;
+    border-radius: 5px;
     width: 100%;
     outline: unset;
 
-    padding:0 40px;
+    padding: 0 40px;
 
     display: flex;
     align-items: center;
@@ -250,15 +229,23 @@
   }
 }
 .navigation {
+  position: sticky;
+  top: -1px; // 0
+  background: #ffffff;
   overflow-x: scroll;
-  margin: 23px 0 20px;
+  margin: 10px 0 20px;
+  // margin-top: 20px;
+  box-shadow: 0px 3px 0px rgba(137, 113, 113, 0.09);
+  height: 45px;
+  padding: 0 0 0 5px !important;
   body::-webkit-scrollbar {
     display: none;
   }
 
-  div {
+  a {
     padding: 6px 9px;
     margin-right: 12px;
+    text-decoration: none;
     cursor: pointer;
     h3 {
       width: max-content;
@@ -281,7 +268,7 @@
   }
 }
 .active {
-  background: #5E5E5E;
+  background: #5e5e5e;
   border-radius: 5px;
 
   h3 {
@@ -292,5 +279,3 @@
   padding: 0 12px;
 }
 </style>
-
-
